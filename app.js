43c79903aa50a28959b38cdacb13e4ddddce37cd
Raul Contreras <rac326@lehigh.edu>
2024-03-31 19:35:43 -0400

    added functionality for the artist dropdown

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+/**
+ * Name: Raul Contreras
+ * ID: RAC326
+ */
 // Put your name and ID here 
 
 const express = require("express");
 const path = require("path");
+const logger = require("morgan");//logger node extension
+
 
 const app = express();
+app.use(logger("dev")); //setting up the logger with morgan
+
+//opening the top40.db using statement from better-sqlite3 github page
+const db = require('better-sqlite3')('top40.db');
+db.pragma('journal_mode = WAL');
 
 app.use(express.static(
   path.resolve(__dirname, "public")
 ));
+
+//router to retrieve a list of unique names and send them to the client
+app.get("/artistList", (req, res) => {
+  const artists = db.prepare('SELECT DISTINCT artist FROM songlist');
+  const artistList = artists.all(); // Execute the prepared statement and fetch all rows
+
+  console.log(artistList); // Log the result to the console
+  res.json(artistList); // Send the result back to the client as JSON
+});
 
 app.listen(3000, () => console.log("Starting up Top 40 Search"));
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,26 @@
+function populateArtists(data){
+    data.forEach(obj =>{
+        const artistName = obj.artist;
+        console.log(artistName);
+        $('#artist').append(`<option id="${artistName}">${artistName}</option>`);
+    })
+}
 
+$( ()=>{//init state
+    //should route '/artistList' and populate the dropdown boxes
+    $.ajax(
+        "/artistList",//returns a json array [{"artist": "artistName"},]
+        {
+            type: "GET",
+            dataType: "json",
+            success: (artists) => {
+                populateArtists(artists);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Error: " + jqXHR.responseText);
+                alert("Error: " + textStatus);
+                alert("Error: " + errorThrown);
+              },
+        }
+    );
+});
