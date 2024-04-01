44bfa300f384edb613e9b26e52e1954b691a5f34
Raul Contreras <rac326@lehigh.edu>
2024-04-01 00:33:40 -0400

    fully functioning app

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const logger = require("morgan");//logger node extension
 const app = express();
 app.use(logger("dev")); //setting up the logger with morgan
 
+
 //opening the top40.db using statement from better-sqlite3 github page
 const db = require('better-sqlite3')('top40.db');
 db.pragma('journal_mode = WAL');
@@ -25,8 +26,43 @@ app.get("/artistList", (req, res) => {
   const artists = db.prepare('SELECT DISTINCT artist FROM songlist');
   const artistList = artists.all(); // Execute the prepared statement and fetch all rows
 
-  console.log(artistList); // Log the result to the console
+  //console.log(artistList); // Log the result to the console
   res.json(artistList); // Send the result back to the client as JSON
+});
+
+app.get("/getSongs/:artistName/:keyWord?", (req, res) =>{
+  /*req.params is a json that returns 
+    {
+  "artistName": "value", -> req.params.artistName
+  "keyWord": "value"     -> req.params.keyWord
+    }
+  */
+  //let me make a prepared Statement 
+  const keyWord = "%" +req.params.keyWord + "%";
+  const artistName = req.params.artistName;
+  console.log(keyWord);
+  console.log(artistName);
+
+  let songs;
+  
+  if(artistName === "All" && !req.params.keyWord){
+    //console.log("line 48");
+    const prep = db.prepare('SELECT * FROM songlist');
+    songs = prep.all();
+  }else if(artistName === "All" && req.params.keyWord){
+    //console.log("line 52");
+    const prep = db.prepare('SELECT * FROM songlist WHERE title LIKE ?');
+     songs = prep.all(keyWord);
+  }else if(artistName != "All" && !req.params.keyWord){
+   // console.log("line 56");
+    const prep = db.prepare('SELECT * FROM songlist WHERE artist = ?');
+    songs = prep.all(artistName);
+  }else{
+    //console.log("line 60");
+    const prep = db.prepare('SELECT * FROM songlist WHERE (artist = ?) AND (title LIKE ?)');
+     songs = prep.all(artistName, keyWord);
+  }
+  res.json(songs);
 });
 
 app.listen(3000, () => console.log("Starting up Top 40 Search"));
--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,65 @@
-function populateArtists(data){
-    data.forEach(obj =>{
-        const artistName = obj.artist;
-        console.log(artistName);
-        $('#artist').append(`<option id="${artistName}">${artistName}</option>`);
-    })
+$(document).ready(() => {
+    console.log("jQuery is ready!");
+
+let songArray = []; //array to hold Song objects I'm creating from json passed from the server
+let indexAt = 0; 
+
+/**
+ * function that views songs in the songArray
+ * 
+ * @param {num} start for start array
+ * @param {num} end for end index
+ */
+function viewSongs(start, end){
+    console.log(start + " " + end);
+    if(start < 0 || start > songArray.length || songArray.length == 0){//checking if we went out of bounds
+        alert("Error: No songs left to view");
+
+    }
+    else{
+        indexAt = getMin(end, songArray.length);
+        for(let i = start; i < indexAt; i++){
+            
+            if(songArray[i].numone){
+                $("#songlist").append(`<tr id="numone"><td>${songArray[i].id}</td><td>${songArray[i].title}</td><td>${songArray[i].artist}</td></tr>`);
+            }else{
+                $("#songlist").append(`<tr><td>${songArray[i].id}</td><td>${songArray[i].title}</td><td>${songArray[i].artist}</td></tr>`);
+            }
+        }
+        $("#tracker").html(`Songs ${(parseInt(start) + 1)} to ${(indexAt)} out of ${songArray.length}`);
+    }
+}
+function getMin(first, second){
+    if(first<second)
+        return first;
+    return second;
 }
 
-$( ()=>{//init state
+$("#search").click(()=>{
+        //event.preventDefault();
+        console.log("clicked");
+    $.ajax(
+        "/getSongs/"+ $("#artist").val() + "/"+ $("#keyword").val(),//route
+        {
+            type: "GET",
+            dataType: "json",
+            success: function (songs){
+                console.log(songs);
+                songArray = songs;
+                $("#songlist").empty();
+                viewSongs(0, $("#songsperpage").val());
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert("Error: " + jqXHR.responseText);
+                alert("Error: " + textStatus);
+                alert("Error: " + errorThrown);
+              },
+            }
+            )
+        });
+        
+   
+        $( ()=>{//init state
     //should route '/artistList' and populate the dropdown boxes
     $.ajax(
         "/artistList",//returns a json array [{"artist": "artistName"},]
@@ -21,6 +74,26 @@ $( ()=>{//init state
                 alert("Error: " + textStatus);
                 alert("Error: " + errorThrown);
               },
-        }
-    );
+            }
+            );
+});
+
+
+
+function populateArtists(data){
+    data.forEach(obj =>{
+        const artistName = obj.artist;
+        //console.log(artistName);
+        $('#artist').append(`<option id="${artistName}">${artistName}</option>`);
+    })
+}
+$("#prev").click(()=>{
+    $("#songlist").empty();
+    viewSongs((parseInt(indexAt) - (parseInt($("#songsperpage").val()) *2)), (parseInt(indexAt) - parseInt($("#songsperpage").val())));
+    });
+$("#next").click(()=>{
+    $("#songlist").empty();
+    
+    viewSongs(indexAt, (parseInt(indexAt) + parseInt($("#songsperpage").val())));
+    });
 });
